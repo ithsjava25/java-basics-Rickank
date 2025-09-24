71d155bfcb86fc38b24a87c9253fd154e70e82eb
Rickard Ankar <rickard.ankar@iths.se>
2025-09-24 10:16:03 +0200

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,9 @@ import com.example.api.ElpriserAPI;
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+
+        elpriserAPI.getPriser("2025-09-24", ElpriserAPI.Prisklass.SE2);
+
+        System.out.print();
     }
 }
